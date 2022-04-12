02f7d38b6c08c38f51a0d3566d36f0e914438f17
Idevaldo <inetomec@yahoo.com.br>
2022-04-12 09:33:02 -0300

    feat: :sparkles: Product

---
+++ src/main/java/com/coursejava/course/services/ProductService.java
@@ -0,0 +1,24 @@
+package com.coursejava.course.services;
+
+import com.coursejava.course.entities.Product;
+import com.coursejava.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
